3d618d19fe6e9aa7bea3a2e9125c5fbd73656448
Otsolain <53212902+Otsolain@users.noreply.github.com>
2026-09-10 10:21:29 +0700

    Create Latihan_1.php

---
+++ pertemuan_2/Latihan_1.php
@@ -0,0 +1,17 @@
+<?php
+$a = 1;
+$b = 2;
+
+$c = $a . $b;
+// Nilai $c sekarang adalah 12 karena angka 1 dan 2 digabungkan.
+
+$d = $c + 1;
+echo "Nilai d = $d<br>";
+// Nilai $d adalah 13.
+
+$e = "Number";
+$f = $e . $d;
+
+echo "Nilai f = $f";
+// Hasil: Number13
+?>
